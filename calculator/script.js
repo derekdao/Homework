c8f46ab6e48ea55a0d110be6dc6a4e7d739875a4
derekdao <daoadung178@gmail.com>
2021-11-23 02:37:15 +0700

    items-list-bt

--- calculator/script.js
+++ calculator/script.js
@@ -1,13 +1,13 @@
-let result = document.getElementById("input-result")
-let calculate=(number)=> {
-    result.value+=number;
+const result = document.getElementById("input-result");
+function clearResult() {
+  result.innerText = "";
 }
-let Result=()=> {
-    result.value=eval(result.value)
-}
-function clear(){
-    result.value = "";
-}
-function del(){
-    result.value = result.value.slice(0,-1)
+let calculate = (number) => {
+  result.value += number;
+};
+let Result = () => {
+  result.value = eval(result.value);
+};
+function del() {
+  result.value = result.value.slice(0, -1);
 }
