bde9a5f227d4775d2722a0b83875c12a9395e598
derekdao <daoadung178@gmail.com>
2021-11-25 13:53:13 +0700

    update calculator

--- calculator/script.js
+++ calculator/script.js
@@ -1,13 +1,16 @@
 const result = document.getElementById("input-result");
-function clearResult() {
-  result.innerText = "";
-}
 let calculate = (number) => {
   result.value += number;
-};
+}
 let Result = () => {
   result.value = eval(result.value);
-};
+}
+function clearResult() {
+  result.value = "";
+}
 function del() {
   result.value = result.value.slice(0, -1);
+}
+function calPercent() {
+  result.value = (result.value)/100
 }
